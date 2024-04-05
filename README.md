bf982c8ab408749f08c418fced9a3e20f8e1c050
patdz <patdz@ymipro.com>
2024-04-05 21:11:12 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # btcconnectapp
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/btcconnectapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.btcconnectapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
